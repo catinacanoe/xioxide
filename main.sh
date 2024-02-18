48da5cec219bd1f2b231a9a6c8739a7cbf7aae9c
catinacanoe <catinacanoe@proton.me>
2024-02-18 01:06:10 -0800

    not even sure what was edited :/

--- main.sh
+++ main.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 case "$1" in
-    "help") cat "$XIOXIDE_PATH/README.md" ;;
-    "config")
+    help|"-h"|"--help") cat "$XIOXIDE_PATH/README.md" ;;
+    config)
 	cat "$XDG_CONFIG_HOME/xioxide/$2.conf"
 	echo "---"
 	cat "$XDG_CONFIG_HOME/xioxide/$2.parsed"
 	;;
-    "reload")
+    conf) cat "$XDG_CONFIG_HOME/xioxide/$2.conf" ;;
+    parsed) cat "$XDG_CONFIG_HOME/xioxide/$2.parsed" ;;
+    reload)
 	if [ -z "$2" ]; then
 	    for file in $XDG_CONFIG_HOME/xioxide/*.conf; do
 	        "$XIOXIDE_PATH/reload.sh" "$file"
@@ -16,7 +18,6 @@ case "$1" in
 	    "$XIOXIDE_PATH/reload.sh" "$XDG_CONFIG_HOME/xioxide/$2.conf"
 	fi
 	;;
-
     *)
 	XIOXIDE_OUTPUT="$("$XIOXIDE_PATH/xioxide.sh" "$2" "$3" "$4" "$5")"
 	[ -z "$1" ] && XIOXIDE_RUNNER="echo" || XIOXIDE_RUNNER="$1"
